ef42f1884c8e402358ff7022afeb91233b758902
Jinil Patel <69380615+jinilpatel25@users.noreply.github.com>
2020-10-26 14:39:44 -0700

    Update dna.c

--- dna.c
+++ dna.c
@@ -6,8 +6,8 @@
               specified sample.  The formatted DNA
               sequence file is a txt file (threes samples
               are provided in the Resource Files folder).
- Author:	Jinil Manish Patel and Aarushi Mehra
- Date: October 2019
+ Author:Jinil Manish Patel and Aarushi Mehra
+ Date:        October 2019
  */
 
  /******************************************************************
